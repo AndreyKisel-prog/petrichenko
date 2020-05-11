9803cf4a2c240d8c45f821116f975a3ce930d64c
AndreyKisel-prog <undrokisel@gmail.com>
2020-05-11 17:10:29 +0500

    HomeWork 2

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-'use strict';
-
-let expenses = {},
-    optionalExpenses,
-    income = [],
-    budgetUser,
-    savings;
-let budget = prompt("Ваш бюджет на месяц?", "30000 рублей");
-let today = prompt("Введите дату в формате YYYY-MM-DD", '2020-05-11');
-
+let money = prompt("Ваш бюджет на месяц?", "30000 рублей"),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '2020-05-11');
 
 let appData = {
-    budgetUser: parseInt(budget, 10),
-    timeData: today,
-    expenses,
-    income,
+    budget: parseInt(money),
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
     savings: false,
 };
 
-
-console.log(budgetUser);
-
 for (let i = 0; i < 2; i++) {
-    let expense = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let expense = +prompt("Введите обязательную статью расходов в этом месяце", "");
     let cost = +prompt("Во сколько это обойдется?", '');
-    expenses.expense = cost;
+
+    if (typeof (expense) === "string"
+        && typeof (expense) != null && typeof (cost) != null
+        && expense != '' && cost != '') {
+        appData.expenses[expense] = cost;
+    } else {
+
+
+    }
 }
 
-alert('Бюджет на 1 день:' + appData.budgetUser / 30 + "рублей");
+
+appData.moneyPerDay = appData.budget / 30;
+alert('Бюджет на 1 день:' + appData.moneyPerDay + "рублей");
 
